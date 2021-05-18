71b29aeb761cfab5abb764282dc1b625a397a1ad
akchindo <abdullahichindo16@gmail.com>
2021-05-18 10:46:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var a ; //Declartion global variable
+var c;
+var d;
+a = 5; //Assignment
+d= 10;
+console.log(a); //Printing a console
+
+var b = "this is javascript" ; //Declartion and Assignment
+
+console.log(b);
+
+c = 100;
+console.log(c);
+
+
+function add() {
+  var e = 20;
+  var f = 30;
+  console.log (a+d)
+}
+
+function sub() {
+  // console.log (e-f)
+}
+
+add();
+
+sub();
