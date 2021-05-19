9946e9d92151ef6d30627c99a51d4b4aa6351f13
akchindo <abdullahichindo16@gmail.com>
2021-05-19 12:26:53 +0100

    fourth commit

---
+++ class2.js
@@ -0,0 +1,46 @@
+// console.log(5 <= 5);
+
+// AND
+
+// x and y | result
+// true and true | true
+// true and false | false
+// false and false | false
+// false and true | false
+
+// OR
+
+// x and y | result
+// true and true | true
+// true and false | true
+// false and false | false
+// false and true | true
+
+// if and else  
+var age = 15;
+var gender = 'm';
+
+// if (age >= 18 && gender == "m"){
+//   console.log("You can join the game cos you are " + age +" years old " )
+// }
+
+// else  {
+//   console.log ('you can go and play at home')
+// }
+
+// if elseif
+if (age >= 18 && gender == "m"){
+  console.log("You can join the game cos you are " + age +" years old " )
+}
+
+else if(age < 18 && age > 10)  {
+  console.log ("you can play ps4 " + age + " years old")
+}
+else 
+  console.log ('you can go and play at home')
+
+
+
+
+
+
