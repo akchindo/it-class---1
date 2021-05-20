626cd78cfb8dbea36631b58a27e612b3a700985b
akchindo <abdullahichindo16@gmail.com>
2021-05-20 09:20:22 +0100

    fifth commit

---
+++ assi-2-greater.js
@@ -0,0 +1,13 @@
+var a = 19
+var b = 18
+
+if (a > b) {
+  console.log(' a is greater than b')
+} else if(a ==b || b ==a) {
+  console.log('they are equal')
+}
+
+else {
+  console.log(' b is greater than a')
+}
+
---
+++ assi2agecomparison.js
@@ -0,0 +1,19 @@
+var ageA = 15
+var ageB = 6
+
+
+if(ageA  >= 18 && ageB >=18 ){
+  console.log (' ageA and ageByou can go to the university')
+}
+else if(ageA >=18 && ageB< 18) {
+  console.log('ageA you can go to the university')
+}
+
+else if(ageB >=18 & ageA< 18) {
+  console.log('ageB you can go to the university')
+}
+
+
+else{
+  console.log(' ageA and ageB you cannot go to the university')
+}
